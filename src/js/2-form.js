d9201fd88c323885bf9956583a4b180312c96667
ValentynaVK <valia2526.vk@gmail.com>
2024-03-18 13:11:05 +0200

    new

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,54 +1,23 @@
-const form = document.querySelector('.feedback-form');
-const messageOfInput = form.elements.message;
-const emailOfInput = form.elements.email;
-const localStorageKey = 'feedback-form-state';
+const KEY_MESSAGE = "feedback-form-state";
+const formRef = document.querySelector('form');
 
-const dataFromLocalStorage = localStorage.getItem(localStorageKey);
-if (dataFromLocalStorage) {
-  try {
-    const parsedData = JSON.parse(dataFromLocalStorage);
-    if (parsedData && typeof parsedData === 'object') {
-      messageOfInput.value = parsedData.message ?? '';
-      emailOfInput.value = parsedData.email ?? '';
-    }
-  } catch (error) {
-    console.error('Error parsing data from local storage:', error);
-  }
+formRef.addEventListener('input', addLocalStorage);
+function addLocalStorage() {
+    const objMessage = JSON.stringify({ email: formRef.elements.email.value, message: formRef.elements.message.value });
+    localStorage.setItem(KEY_MESSAGE, objMessage);    
 }
 
-form.addEventListener('input', saveData);
-form.addEventListener('submit', uploadData);
+document.addEventListener('DOMContentLoaded', () => {
+    const objMessage = JSON.parse(localStorage.getItem(KEY_MESSAGE)) || {};
+    formRef.elements.email.value = objMessage.email || '';
+    formRef.elements.message.value = objMessage.message || '';
+});
 
-function saveData() {
-  const dataOfInput = {
-    email: emailOfInput.value,
-    message: messageOfInput.value,
-  };
-  const trimedDataOfInput = trimValues(dataOfInput);
-  localStorage.setItem(localStorageKey, JSON.stringify(trimedDataOfInput));
-}
-
-function trimValues(obj) {
-  const arrOfKeyVal = Object.entries(obj);
-  let trimedObj = {};
-  for (const [key, value] of arrOfKeyVal) {
-    trimedObj[key] = value.toString().trim();
-  }
-  return trimedObj;
-}
-
-function uploadData(evt) {
-  evt.preventDefault();
-  const email = evt.target.elements.email.value.trim();
-  const message = evt.target.elements.message.value.trim();
-  if (email === '' || message === '') {
-    alert('Please complete all the fields!');
-    return;
-  }
-  console.log({
-    email,
-    message,
-  });
-  localStorage.removeItem(localStorageKey);
-  form.reset();
+formRef.addEventListener('submit', removeLocalStorage);
+function removeLocalStorage(event) {
+    event.preventDefault(); 
+    console.log(JSON.parse(localStorage.getItem(KEY_MESSAGE)));
+    localStorage.removeItem(KEY_MESSAGE);
+    formRef.elements.email.value = '';
+    formRef.elements.message.value = '';
 }
